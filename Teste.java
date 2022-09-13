4c2e5f429f42e25d5324251a843646c659172ae7
Davi Paulino <davipm@hotmail.com>
2022-09-13 13:34:29 -0300

    Loteria 1.0

--- Teste.java
+++ Teste.java
@@ -1,35 +1,30 @@
-import java.util.Random;
 import java.util.Scanner;
+
+import entities.Bilhete;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		Random rand = new Random();
 		Scanner sc = new Scanner(System.in);
+		Bilhete b;
 
 		boolean loop = true;
 
 		while (loop == true) {
 
-			System.out.print("Quer um ticket? [s]/[n] ");
+			System.out.print("Quer um blihete da sorte? [s]/[n] ");
 			String resp = sc.next();
 
 			if (resp.equalsIgnoreCase("s")) {
+				b = new Bilhete();
 				System.out.println("\nSeus números são: ");
-				StringBuffer sb = new StringBuffer();
-				for (int i = 0; i < 5; i++) {
-					sb.append(rand.nextInt(0, 61));
-					if (i < 4) {
-						sb.append("-");
-					}
-				}
-				System.out.println(sb + "\n");
+				System.out.println(b.sortear());
 
 			} else {
 				break;
 
 			}
-			
+
 		}
 		sc.close();
 	}
---
+++ entities/Bilhete.java
@@ -0,0 +1,19 @@
+package entities;
+
+import java.util.Random;
+
+public class Bilhete {
+	Random rand = new Random();
+	StringBuffer sb=new StringBuffer();
+	
+	public String sortear() {
+		for(int i=0;i<5;i++) {
+			sb.append(rand.nextInt(0,61));
+			if(i<4) {
+				sb.append("-");
+			}
+		}
+		return sb.toString();
+	}
+	
+}
